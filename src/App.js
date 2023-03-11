57313dbb80f36eaba254d001fab2e70d898fe411
OlgaAbdu <olgasavik84@gmail.com>
2023-03-10 18:41:08 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import image from './image_processing20200713-19777-12obfar.png';
 
-function App() {
+function App () {
+  const[count,setCount] =useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container my-5'>
+      <img src={image} alt='numbers' style={{ maxWidth: '100%' }} >
+      </img>
+      <div className='card text-center my-5'>
+        <div className='card-body'>
+          <h1> Counter App</h1>
+          <div className='my-5'>
+            <h2 className='my-5'>{count}</h2>
+            <button className='btn btn-outline-success mx-3' onClick={() => setCount(count + 1)}>Increment</button>
+            <button className='btn btn-outline-danger mx-3'onClick={() => setCount(count - 1 )}>Decrement</button>
+            <button className='btn btn-outline-secondary mx-3' onClick={() => setCount(0)}>Clear</button>
+
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
